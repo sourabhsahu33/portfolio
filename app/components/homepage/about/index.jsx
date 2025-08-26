06fce30e9435dc040cac78c7692b2d3cf5e43a83
Sourabh Sahu <104298270+sourabhsahu33@users.noreply.github.com>
2025-08-26 11:13:28 +0530

    Update index.jsx

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -17,7 +17,7 @@ function AboutSection() {
         <div className="flex justify-center order-1 lg:order-2">
           <Image
             src={personalData.profile}
-            width={280}
+            width={320}
             height={280}
             alt="Abu Said"
             className="rounded-lg transition-all duration-1000 grayscale hover:grayscale-0 hover:scale-110 cursor-pointer"
